b0476891c5030622b65d6823dcef6909f2321a0e
Jardel Costa <jfcosta@ic.ufal.br>
2018-10-21 22:12:44 -0300

    view para o GET

--- polls/views.py
+++ polls/views.py
@@ -6,7 +6,10 @@ from .models import Subject
 
 def index(request):
     output = ', '.join([s.nameSubject for s in Subject.objects])
-    return HttpResponse(output)
+    context = {
+        'Subject.objects': Subject.objects,
+    }
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, idSubject):
